b0cd9336d985d93cf61ef222aeefa74918f15aff
Jon Bomgardner <Jbomgard@gmail.com>
2015-11-28 22:27:45 +0000

    add handlebars helpers.js

---
+++ config/helper.js
@@ -0,0 +1,9 @@
+var handlebars = require('sails/node_modules/express-handlebars/node_modules/handlebars'),
+    layouts = require('handlebars-layouts'),
+    forms = require('handlebars-form-helpers'),
+    fs = require('fs');
+    
+forms.register(handlebars);
+layouts.register(handlebars);
+
+handlebars.registerPartial('layout', fs.readFileSync('../views/layouts/layout.hbs', 'utf8'));
